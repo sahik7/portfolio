95f36f963d0a2a06fd8c82bfae3aa2f6d0f98aac
sahik7 <sahikahsan7@gmail.com>
2024-01-10 21:12:25 +0600

    pdf download functionality created

--- app/components/Button.js
+++ app/components/Button.js
@@ -2,7 +2,22 @@
 import React from 'react'
 
 export default function Button() {
+    const downloadResume = () => {
+        fetch("sahik_mueeb_ahsan.pdf").then((response) => {
+            response.blob().then((blob) => {
+
+                const fileURL =
+                    window.URL.createObjectURL(blob);
+
+                // Setting various property values
+                let alink = document.createElement("a");
+                alink.href = fileURL;
+                alink.download = "sahik_mueeb_ahsan.pdf";
+                alink.click();
+            });
+        });
+    };
     return (
-        <button className="py-2 px-4 bg-[#f1491c] font-medium transition font-work_sans ease-in hover:scale-105">Download Resume</button>
+        <button onClick={downloadResume} className="py-2 px-4 bg-[#f1491c] font-medium font-work_sans hover:py-3 hover:px-6">Download Resume</button>
     )
 }
